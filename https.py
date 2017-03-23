078e60d096de9aba6a2a2b5a4bc168bfeca06c10
benjimin <ben.lewis@ga.gov.au>
2017-03-23 13:53:38 +1100

    simple https example

--- https.py
+++ https.py
@@ -25,7 +25,11 @@ assert r.status == 407 # ---- knock is complete
 import sspiauth
 credentials = sspiauth.sspi_ntlm_auth()
 
-conn = httplib.HTTPConnection(proxyhost, proxyport)
+# refresh connection
+import socket
+conn.sock = socket.create_connection((proxyhost, proxyport), conn.timeout, conn.source_address)
+#conn = httplib.HTTPConnection(proxyhost, proxyport) # can we achieve this without actually doing this?
+
 conn.request('CONNECT', tunnel_resource, headers={'Proxy-Authorization':credentials()}) # Negotiate.
 
 conn._method = 'HEAD' # response class should not try to consume a message body
@@ -36,11 +40,11 @@ r.read() # change state (finish with this response to permit the next)
 assert challenge.startswith('NTLM ') # note, this assertion is excessively restrictive; ".contains" might be preferable
 assert r.getheader('Connection').lower() == 'Keep-Alive'.lower()
 
-conn.request('CONNECT', tunnel_resource, headers={'Proxy-Authorization':credentials(challenge)}) 
+conn.request('CONNECT', tunnel_resource, headers={'Proxy-Authorization':credentials(challenge)})
 conn._method = 'HEAD' # response class should not try to consume a message body
 r = conn.getresponse()
 r.read() # change state
-
+#r.read(amt=0)
 assert r.status == 200
 
 conn2 = httplib.HTTPSConnection(desthost, destport) # Now switch to Secure
